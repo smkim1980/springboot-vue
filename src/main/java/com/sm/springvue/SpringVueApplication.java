14d44b8d0608d914081506fd2ab42f7d2c82cc9a
LAPTOP-D1O1MRJB\김순모 <rlatnsah@gmail.com>
2023-02-16 20:42:19 +0900

    #application update && PID File Write 생성

--- src/main/java/com/sm/springvue/SpringVueApplication.java
+++ src/main/java/com/sm/springvue/SpringVueApplication.java
@@ -2,12 +2,15 @@ package com.sm.springvue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringVueApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringVueApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(SpringVueApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 }
