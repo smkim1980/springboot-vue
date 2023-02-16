416e913c0ae7dea5812ce9ec49367a078ca73354
LAPTOP-D1O1MRJB\김순모 <rlatnsah@gmail.com>
2023-02-16 21:42:14 +0900

    #프로퍼티 추가

---
+++ src/main/java/com/sm/springvue/SpringVueController.java
@@ -0,0 +1,33 @@
+package com.sm.springvue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class SpringVueController {
+
+    @Value("${spring.pid.file}")
+    String springPidFileName;
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+
+    @GetMapping
+    public Map<String ,String > getYml(){
+
+        log.info("pid={},profile={}",springPidFileName,activeProfile);
+
+        Map<String,String> result = new HashMap<>();
+        result.put("springPidFileName" , springPidFileName);
+        result.put("activeProfile" , activeProfile);
+
+        return result;
+    }
+}
